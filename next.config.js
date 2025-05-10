25541a0f5616d553b413f107c5fe51a7a3b7df82
LSWasTaken <heavenlyheadband@gmail.com>
2025-05-10 09:27:04 +0800

    fix: resolve undici webpack error

--- next.config.js
+++ next.config.js
@@ -5,14 +5,23 @@ const nextConfig = {
   images: {
     domains: ['avatars.githubusercontent.com'],
   },
-  webpack: (config) => {
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      fs: false,
-      net: false,
-      tls: false,
-      crypto: false,
-    };
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        net: false,
+        tls: false,
+        crypto: false,
+        stream: false,
+        http: false,
+        https: false,
+        zlib: false,
+        path: false,
+        os: false,
+        util: false,
+      };
+    }
     return config;
   },
 };
